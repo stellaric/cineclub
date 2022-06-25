0684d250840776185613fe45a0007781819affe2
Stella <90323575+stellaric@users.noreply.github.com>
2022-06-25 18:41:45 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cineclub
-projet fait en terminale , modifier aujourdhui 
+Ce repository est composée d'un site vitrine fait en terminale (en groupe)
+Ce site n'est pas très récent et  va bientôt être actualiser 
---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+require('sources/en-tete.php');
+?>
+
+<body class="layout">
+ 
+<nav class="navbar shadow-5 black " >
+  <a href="#" class="navbar-logo navbar-brand  ">
+     <img src="images/logo.png" alt="Xelzs" />
+   </a>
+
+  <div class="navbar-menu ml-auto"> 
+    <a class="navbar-link" style="background-color: #ED9D01;"href="vues/connexion.php">S'Identifier</a>
+    
+  </div>
+
+</nav>
+
+
+
+<div class="grix xs2 ">
+  <div class="pos-xs1" >
+  <video  style="  width: 201%;" id="background-video"  autoplay loop muted>
+    <source src="images/pexels-cottonbro-8262686.mp4" type="video/mp4" ">
+  </video>
+  
+</div>
+    <div class="pos-xs2" style="background-color: #ED9D01;opacity: 48%;margin-left: 180px;height:98%;">
+    
+<center><img src="images/logo_sf.png" alt="logo" width="500" height="200"></center>
+
+  
+
+
+   
+ 
+
+ <div class="card shadow-1  rounded-3 black" style="margin: 5%;">
+  <div class="card-header">Card header</div>
+  <div class="card-content">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Non veritatis excepturi natus impedit distinctio quidem, nesciunt id
+      possimus quia, tempore fuga doloribus reiciendis facilis voluptate accusantium! Adipisci est nam soluta!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Non veritatis excepturi natus impedit distinctio quidem, nesciunt id
+      possimus quia, tempore fuga doloribus reiciendis facilis voluptate accusantium! Adipisci est nam soluta!
+    </p>
+  </div>
+</div>
+
+
+
+<center><p><b><a href="default.asp"  style="    text-align: center;
+    text-decoration-line: revert;">This is a link ?</a></b></p></center>
+  </div>  
+  </div>
+</div>
+
+
+
+
+
+<?php
+require('sources/footer.php');
+?>
+
+
+</body>
+</html>
